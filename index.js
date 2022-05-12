19a9f11504ccd338974ad8c2cd1ecf8596ea9992
Patrick Young <patrickyoung281@gmail.com>
2022-05-12 12:41:59 -0400

    commit tree 53c02d96a04874acb1f0f61cc349237102903c97

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+function divide (a=2000,b=100) {
+    return a/b;
+}
+
+const square = num1 => num1**2;
+
+
+const add = (num1, num2) => num1+num2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+function divide (a=2000, b=100) {
+    return a/b 
+};
+
+const square = number => number**2;
+
+const add = (a, b) => a+b;
+
+*/
